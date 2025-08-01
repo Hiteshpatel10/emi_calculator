b9d01112d5ce378e48ace2766f9fa139c7dad9fe
Hitesh Patel <hpatel.hp2001@gmail.com>
2025-08-01 13:57:56 +0530

    emi

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,18 @@
 
-import 'package:core_calculator/utils/core_calculator_bloc_init.dart';
-import 'package:core_calculator/utils/core_calculator_route_generator.dart';
-import 'package:core_utility/core_theme.dart';
+import 'package:core_calculator/home_module/view/calculator_my_app.dart';
 import 'package:core_utility/core_utility.dart';
-import 'package:core_utility/navigation/core_calculator_routes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'navigation/route_generator.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      // options: const FirebaseOptions(
-      //   apiKey: 'AIzaSyCdJoD0lKc_I-BmX9Gh6PigzjyHbo0Fygk',
-      //   appId: '1:393406614769:android:db3aa5f337004c98f36d50',
-      //   projectId: 'gita-sarathi',
-      //   messagingSenderId: '',
-      // ),
-      );
-
+  await Firebase.initializeApp();
   await initGlobalKeys(navigatorKey, scaffoldMessengerKey, baseUrl: '');
 
   FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(!kDebugMode);
@@ -37,33 +24,6 @@ void main() async {
     return true;
   };
 
-  runApp(const MyApp());
+  runApp(const CalculatorMyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        ...coreCalculatorBlocInit(),
-        // ...locatorBlocInit(),
-      ],
-      child: MaterialApp(
-        title: 'EMI Calculator',
-        scaffoldMessengerKey: scaffoldMessengerKey,
-        navigatorKey: navigatorKey,
-        debugShowCheckedModeBanner: false,
-        initialRoute: CoreCalculatorRoutes.emiHome,
-        onGenerateRoute: (settings) {
-          return RouteGenerator.buildRouteGenerator(settings, {
-            // "/locator": locatorRouteGenerator(settings),
-            "/calculator": coreCalculatorRouteGenerator(settings),
-          });
-        },
-        theme: CoreAppTheme.theme(context),
-      ),
-    );
-  }
-}
